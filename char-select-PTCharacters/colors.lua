e60dcd60b93c8cf2f015eefb018c5269fe795de6
SKL <37816816+SuperKirbylover@users.noreply.github.com>
2025-04-07 15:32:40 -0400

    Create colors.lua

---
+++ char-select-PTCharacters/colors.lua
@@ -0,0 +1,451 @@
+-- [[since keeping the palettes all in main gets it messy VERY quickly, i'm just posting it all here
+PP_PALETTES = {
+	["peppino"] = {
+		[1] = { -- DEFAULT (Classic Cook)
+			[PANTS] = "000000",
+			[SHIRT] = "ffffff", -- tanktop
+			[GLOVES] = "000000", -- shirt
+			[SHOES] = "000000",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "ffffff",
+			[EMBLEM] = "ffffff"
+		},
+		[2] = { -- UNFUNNY COOK
+			[PANTS] = "000000",
+			[SHIRT] = "88a8c8", -- tanktop
+			[GLOVES] = "000000", -- shirt
+			[SHOES] = "000000",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "88a8c8",
+			[EMBLEM] = "88a8c8"
+		},
+		[3] = { -- MONEY GREEN
+			[PANTS] = "000000",
+			[SHIRT] = "58c000", -- tanktop
+			[GLOVES] = "000000", -- shirt
+			[SHOES] = "000000",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "58c000",
+			[EMBLEM] = "58c000"
+		},
+		[4] = { -- SAGE BLUE
+			[PANTS] = "000000",
+			[SHIRT] = "30a8f8", -- tanktop
+			[GLOVES] = "000000", -- shirt
+			[SHOES] = "000000",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "30a8f8",
+			[EMBLEM] = "30a8f8"
+		},
+		[5] = { -- BLOOD RED
+			[PANTS] = "000000",
+			[SHIRT] = "e03000", -- tanktop
+			[GLOVES] = "000000", -- shirt
+			[SHOES] = "000000",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "e03000",
+			[EMBLEM] = "e03000"
+		},
+		[6] = { -- TV PURPLE
+			[PANTS] = "000000",
+			[SHIRT] = "9850f8", -- tanktop
+			[GLOVES] = "000000", -- shirt
+			[SHOES] = "000000",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "9850f8",
+			[EMBLEM] = "9850f8"
+		},
+		[7] = { -- DARK COOK
+			[PANTS] = "000000",
+			[SHIRT] = "000000", -- tanktop
+			[GLOVES] = "000000", -- shirt
+			[SHOES] = "000000",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "000000",
+			[EMBLEM] = "000000"
+		},
+		[8] = { -- SHITTY COOK
+			[PANTS] = "000000",
+			[SHIRT] = "c08038", -- tanktop
+			[GLOVES] = "000000", -- shirt
+			[SHOES] = "000000",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "c08038",
+			[EMBLEM] = "c08038"
+		},
+		[9] = { -- GOLDEN GOD
+			[PANTS] = "000000",
+			[SHIRT] = "e09000", -- tanktop
+			[GLOVES] = "000000", -- shirt
+			[SHOES] = "000000",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "e09000",
+			[EMBLEM] = "e09000"
+		},
+		[10] = { -- GARISH COOK
+			[PANTS] = "000000",
+			[SHIRT] = "e85098", -- tanktop
+			[GLOVES] = "000000", -- shirt
+			[SHOES] = "000000",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "e85098",
+			[EMBLEM] = "e85098"
+		},
+		[11] = { -- MOONEY ORANGE
+			[PANTS] = "000000",
+			[SHIRT] = "f87018", -- tanktop
+			[GLOVES] = "000000", -- shirt
+			[SHOES] = "000000",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "f87018",
+			[EMBLEM] = "f87018"
+		},
+		[12] = { -- PEDDITO
+			[PANTS] = "e03000",
+			[SHIRT] = "d868a0", -- tanktop
+			[GLOVES] = "0068e8", -- shirt
+			[SHOES] = "e03000",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "d868a0",
+			[EMBLEM] = "d868a0"
+		},
+	},
+	["gustavo"] = {
+		[1] = { -- DEFAULT
+			[PANTS] = "a03800",
+			[SHIRT] = "a03800",
+			[GLOVES] = "ffffff", -- apron
+			[SHOES] = "a03800",
+			[HAIR] = "a03800",
+			[SKIN] = "ffaa83",
+			[CAP] = "ffffff",
+			[EMBLEM] = "a03800",
+		},
+		[2] = { -- UNFUNNY COOK
+			[PANTS] = "a03800",
+			[SHIRT] = "a03800",
+			[GLOVES] = "88a8c8", -- apron
+			[SHOES] = "a03800",
+			[HAIR] = "a03800",
+			[SKIN] = "ffaa83",
+			[CAP] = "88a8c8",
+			[EMBLEM] = "88a8c8"
+		},
+		[3] = { -- MONEY GREEN
+			[PANTS] = "a03800",
+			[SHIRT] = "a03800",
+			[GLOVES] = "58c000", -- apron
+			[SHOES] = "a03800",
+			[HAIR] = "a03800",
+			[SKIN] = "ffaa83",
+			[CAP] = "58c000",
+			[EMBLEM] = "58c000"
+		},
+		[4] = { -- SAGE BLUE
+			[PANTS] = "a03800",
+			[SHIRT] = "a03800",
+			[GLOVES] = "30a8f8", -- apron
+			[SHOES] = "a03800",
+			[HAIR] = "a03800",
+			[SKIN] = "ffaa83",
+			[CAP] = "30a8f8",
+			[EMBLEM] = "30a8f8"
+		},
+		[5] = { -- BLOOD RED
+			[PANTS] = "a03800",
+			[SHIRT] = "a03800",
+			[GLOVES] = "e03000", -- apron
+			[SHOES] = "a03800",
+			[HAIR] = "a03800",
+			[SKIN] = "ffaa83",
+			[CAP] = "e03000",
+			[EMBLEM] = "e03000"
+		},
+		[6] = { -- TV PURPLE
+			[PANTS] = "a03800",
+			[SHIRT] = "a03800",
+			[GLOVES] = "9850f8", -- apron
+			[SHOES] = "a03800",
+			[HAIR] = "a03800",
+			[SKIN] = "ffaa83",
+			[CAP] = "9850f8",
+			[EMBLEM] = "9850f8"
+		},
+		[7] = { -- DARK COOK
+			[PANTS] = "a03800",
+			[SHIRT] = "a03800",
+			[GLOVES] = "000000", -- apron
+			[SHOES] = "a03800",
+			[HAIR] = "a03800",
+			[SKIN] = "ffaa83",
+			[CAP] = "000000",
+			[EMBLEM] = "000000"
+		},
+		[8] = { -- SHITTY COOK
+			[PANTS] = "a03800",
+			[SHIRT] = "a03800",
+			[GLOVES] = "c08038", -- apron
+			[SHOES] = "a03800",
+			[HAIR] = "a03800",
+			[SKIN] = "ffaa83",
+			[CAP] = "c08038",
+			[EMBLEM] = "c08038"
+		},
+		[9] = { -- GOLDEN GOD
+			[PANTS] = "a03800",
+			[SHIRT] = "a03800",
+			[GLOVES] = "e09000", -- apron
+			[SHOES] = "a03800",
+			[HAIR] = "a03800",
+			[SKIN] = "ffaa83",
+			[CAP] = "e09000",
+			[EMBLEM] = "e09000"
+		},
+		[10] = { -- GARISH COOK
+			[PANTS] = "a03800",
+			[SHIRT] = "a03800",
+			[GLOVES] = "e85098", -- apron
+			[SHOES] = "a03800",
+			[HAIR] = "a03800",
+			[SKIN] = "ffaa83",
+			[CAP] = "e85098",
+			[EMBLEM] = "e85098"
+		},
+		[11] = { -- MOONEY ORANGE
+			[PANTS] = "a03800",
+			[SHIRT] = "a03800",
+			[GLOVES] = "f87018", -- apron
+			[SHOES] = "a03800",
+			[HAIR] = "a03800",
+			[SKIN] = "ffaa83",
+			[CAP] = "f87018",
+			[EMBLEM] = "f87018"
+		},
+		[11] = { -- CRUSTAVO
+			[PANTS] = "d89060",
+			[SHIRT] = "d89060",
+			[GLOVES] = "30a8f8", -- apron
+			[SHOES] = "d89060",
+			[HAIR] = "58c000",
+			[SKIN] = "f8e080",
+			[CAP] = "30a8f8",
+			[EMBLEM] = "58c000"
+		}
+	},
+	["noise"] = {
+		[1] = { -- DEFAULT (Noise)
+			[PANTS] = "f8e080",
+			[SHIRT] = "d88818", -- cape
+			[GLOVES] = "ffffff",
+			[SHOES] = "f8e080",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "f8e080",
+			[EMBLEM] = "d88818"
+		},
+		[2] = { -- BOISE
+			[PANTS] = "30a8f8",
+			[SHIRT] = "d88818", -- cape
+			[GLOVES] = "ffffff",
+			[SHOES] = "30a8f8",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "30a8f8",
+			[EMBLEM] = "d88818"
+		},
+		[3] = { -- ROISE
+			[PANTS] = "f80000",
+			[SHIRT] = "d88818", -- cape
+			[GLOVES] = "ffffff",
+			[SHOES] = "f80000",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "f80000",
+			[EMBLEM] = "d88818"
+		},
+		[4] = { -- POISE
+			[PANTS] = "9850f8",
+			[SHIRT] = "d88818", -- cape
+			[GLOVES] = "ffffff",
+			[SHOES] = "9850f8",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "9850f8",
+			[EMBLEM] = "d88818"
+		},
+		[5] = { -- REVERSE
+			[PANTS] = "d88818",
+			[SHIRT] = "f8e080", -- cape
+			[GLOVES] = "ffffff",
+			[SHOES] = "d88818",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "d88818",
+			[EMBLEM] = "f8e080"
+		},
+		[6] = { -- CRITIC
+			[PANTS] = "e03000",
+			[SHIRT] = "60d048", -- cape
+			[GLOVES] = "ffffff",
+			[SHOES] = "e03000",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "e03000",
+			[EMBLEM] = "60d048"
+		},
+		[7] = { -- OUTLAW
+			[PANTS] = "982800",
+			[SHIRT] = "d88818", -- cape
+			[GLOVES] = "ffffff",
+			[SHOES] = "982800",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "982800",
+			[EMBLEM] = "d88818"
+		},
+		[8] = { -- ANTI-DOISE
+			[PANTS] = "d868a0",
+			[SHIRT] = "48a0f8", -- cape
+			[GLOVES] = "ffffff",
+			[SHOES] = "d868a0",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "d868a0",
+			[EMBLEM] = "48a0f8"
+		},
+		[9] = { -- FLESH EATER
+			[PANTS] = "ffaa83",
+			[SHIRT] = "ffffff", -- cape
+			[GLOVES] = "ffffff",
+			[SHOES] = "ffaa83",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "ffaa83",
+			[EMBLEM] = "ffffff"
+		},
+		[10] = { -- SUPER
+			[PANTS] = "ffffff",
+			[SHIRT] = "f8e080", -- cape
+			[GLOVES] = "ffffff",
+			[SHOES] = "ffffff",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "ffffff",
+			[EMBLEM] = "f8e080"
+		},
+		[11] = { -- FAST PORCUPINE
+			[PANTS] = "9850f8",
+			[SHIRT] = "f80000", -- cape
+			[GLOVES] = "ffffff",
+			[SHOES] = "9850f8",
+			[HAIR] = "000000",
+			[SKIN] = "9850f8",
+			[CAP] = "9850f8",
+			[EMBLEM] = "f80000"
+		},
+		[12] = { -- FEMININE SIDE
+			[PANTS] = "e85098",
+			[SHIRT] = "f80000", -- cape
+			[GLOVES] = "ffffff",
+			[SHOES] = "e85098",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "e85098",
+			[EMBLEM] = "f80000"
+		},
+		[13] = { -- REAL DOISE
+			[PANTS] = "48a0f8",
+			[SHIRT] = "d868a0", -- cape
+			[GLOVES] = "ffffff",
+			[SHOES] = "48a0f8",
+			[HAIR] = "000000",
+			[SKIN] = "c08038",
+			[CAP] = "48a0f8",
+			[EMBLEM] = "d868a0"
+		},
+		[14] = { -- FOREST GOBLIN
+			[PANTS] = "982800",
+			[SHIRT] = "f80000", -- cape
+			[GLOVES] = "ffffff",
+			[SHOES] = "982800",
+			[HAIR] = "000000",
+			[SKIN] = "60d048",
+			[CAP] = "982800",
+			[EMBLEM] = "f80000"
+		},
+		[15] = { -- DOISE (robby1scool)
+			[PANTS] = "64c9ff",
+			[SHIRT] = "fd1ffe", -- cape
+			[GLOVES] = "0019ff",
+			[SHOES] = "64c9ff",
+			[HAIR] = "000000",
+			[SKIN] = "c96205",
+			[CAP] = "64c9ff",
+			[EMBLEM] = "64c9ff"
+		}
+	},
+	["noisette"] = {
+		[1] = { -- DEFAULT
+			[PANTS] = "e85098",
+			[SHIRT] = "ffffff",
+			[GLOVES] = "ffffff",
+			[SHOES] = "f02425",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "e85098",
+			[EMBLEM] = "e85098"
+		},
+		[2] = { -- PEPPINO & NOISE COMICS
+			[PANTS] = "f8d0d0",
+			[SHIRT] = "f80000",
+			[GLOVES] = "ffffff",
+			[SHOES] = "f80000",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "f8d0d0",
+			[EMBLEM] = "f8d0d0"
+		},
+		[3] = { -- TABLETOP
+			[PANTS] = "f80000",
+			[SHIRT] = "d868a0",
+			[GLOVES] = "ffffff",
+			[SHOES] = "f80000",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "d868a0",
+			[EMBLEM] = "d868a0"
+		},
+		[4] = { -- TOPPINBOT PORTRAIT (PURPLE)
+			[PANTS] = "aa4cf3",
+			[SHIRT] = "600000",
+			[GLOVES] = "ffffff",
+			[SHOES] = "600000",
+			[HAIR] = "000000",
+			[SKIN] = "ffaa83",
+			[CAP] = "f8d0d0",
+			[EMBLEM] = "aa4cf3"
+		},
+		[5] = { -- DOISETTE (robby1scool)
+			[PANTS] = "d481ff",
+			[SHIRT] = "ffffff",
+			[GLOVES] = "0002fc",
+			[SHOES] = "e0942b",
+			[HAIR] = "5a341b",
+			[SKIN] = "db5c00",
+			[CAP] = "d481ff",
+			[EMBLEM] = "aa4cf3"
+		},
+	}
+};
